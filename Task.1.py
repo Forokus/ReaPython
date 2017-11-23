19c57cf7d928dd3f02ac848d24b1a9477c437fff
Forokus <33478835+Forokus@users.noreply.github.com>
2017-11-23 13:31:11 +0300

    Delete Task.1.py

--- Task.1.py
+++
@@ -1,7 +0,0 @@
-import tmdb
-from os import environ
-
-my_api_key = environ['tmdbKey']
-print('Введите номер фильма:')
-n = int(input())
-print('Бюджет фильма составляет:' + str(tmdb.get_movie_details(n, my_api_key)['budget']))
